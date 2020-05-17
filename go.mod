ee882713fd1ef1aec60a0f7d44800125600106dc
liangchangyou <liangchangyoujackson@gmail.com>
2020-05-17 08:40:46 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jacksonyoudi/gomodone
+
+go 1.14
---
+++ say.go
@@ -0,0 +1,8 @@
+package gomodone
+
+import "fmt"
+
+// Hi returns a friendly greeting
+func SayHi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
